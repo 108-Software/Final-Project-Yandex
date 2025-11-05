299de88f1ab0d9a7c2bfce541ea3f0c40dae48bf
108-Software <75811746+108-Software@users.noreply.github.com>
2025-11-05 11:41:19 +0300

    Update api.go

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -2,11 +2,24 @@ package api
 
 import "github.com/gin-gonic/gin"
 
+type TaskResponse struct {
+    ID      string `json:"id"`
+    Date    string `json:"date"`
+    Title   string `json:"title"`
+    Comment string `json:"comment"`
+    Repeat  string `json:"repeat"`
+}
+
+type TasksResp struct {
+    Tasks []*TaskResponse `json:"tasks"`
+}
+
 func Init(r *gin.Engine) {
-	// Создаем группу для API маршрутов
+	//Группа для API маршрутов
 	apiGroup := r.Group("/api")
 	{
 		apiGroup.GET("/nextdate", NextDateHandler)
+		apiGroup.GET("/tasks", TasksHandler)
 		apiGroup.POST("/task", AddTaskHandler)
 	}
 
