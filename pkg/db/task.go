fe61dd450abf5d88d4e6f17f5d3c79c91bb484b5
108-Software <75811746+108-Software@users.noreply.github.com>
2025-11-05 11:39:52 +0300

    Update task.go

--- pkg/db/task.go
+++ pkg/db/task.go
@@ -1,40 +1,35 @@
 package db
 
 import (
-	"time"
+    "time"
 )
 
-// Task структура задачи
 type Task struct {
-	ID      int64  `json:"id"`
-	Date    string `json:"date"`
-	Title   string `json:"title"`
-	Comment string `json:"comment"`
-	Repeat  string `json:"repeat"`
+    ID      int    `json:"id"`
+    Date    string `json:"date"`
+    Title   string `json:"title"`
+    Comment string `json:"comment"`
+    Repeat  string `json:"repeat"`
 }
 
-// AddTask добавляет задачу в базу данных
 func AddTask(task *Task) (int64, error) {
-	// Выполняем SQL запрос
-	result, err := db.Exec(`
-		INSERT INTO scheduler (date, title, comment, repeat) 
-		VALUES (?, ?, ?, ?)
-	`, task.Date, task.Title, task.Comment, task.Repeat)
-	if err != nil {
-		return 0, err
-	}
-	
-	// Получаем ID последней вставленной записи
-	id, err := result.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
-	
-	return id, nil
+    result, err := DB.Exec(
+        `INSERT INTO scheduler (date, title, comment, repeat) 
+         VALUES (?, ?, ?, ?)`,
+        task.Date, task.Title, task.Comment, task.Repeat)
+    if err != nil {
+        return 0, err
+    }
+    
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, err
+    }
+    
+    return id, nil
 }
 
-// Вспомогательная функция для проверки даты
 func IsValidDate(dateStr string) bool {
-	_, err := time.Parse("20060102", dateStr)
-	return err == nil
+    _, err := time.Parse("20060102", dateStr)
+    return err == nil
 }
